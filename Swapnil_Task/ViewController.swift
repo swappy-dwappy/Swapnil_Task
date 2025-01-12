1bcfc9ca28fffb88b3ef4903fa671a23e226410d
swappy-dwappy <rit2010016@gmail.com>
2025-01-12 15:53:22 +0530

    Initial commit

---
+++ Swapnil_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swapnil_Task
+//
+//  Created by Sonkar, Swapnil on 11/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swapnil_TaskUITests/Swapnil_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swapnil_TaskUITestsLaunchTests.swift
+//  Swapnil_TaskUITests
+//
+//  Created by Sonkar, Swapnil on 11/01/25.
+//
+
+import XCTest
+
+final class Swapnil_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
